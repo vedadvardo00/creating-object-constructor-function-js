500bfde400839b94766d2b6223fe101157587459
Vedad Vardo <vedadvardo00@gmail.com>
2025-06-12 18:59:20 +0200

    Adding function inside constructor function

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function HouseKeeper (name, workLicence, yearsOfExperience, cleaningRepetoire){
     this.workLicence = workLicence;
     this.yearsOfExperience = yearsOfExperience;
     this.cleaningRepetoire = cleaningRepetoire;
+    this.clean = function () {
+        alert("Cleaning in progress...");
+    }
 }
 
 var housekeeper1 = new HouseKeeper ('Hanuma', true, 3, ['bathroom', 'hall']);
