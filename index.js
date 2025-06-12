16ad092cf042066b16141114523aa7701df4926a
Vedad Vardo <vedadvardo00@gmail.com>
2025-06-12 18:48:06 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function HouseKeeper (name, workLicence, yearsOfExperience, cleaningRepetoire){
+    this.name = name;
+    this.workLicence = workLicence;
+    this.yearsOfExperience = yearsOfExperience;
+    this.cleaningRepetoire = cleaningRepetoire;
+}
+
+var housekeeper1 = new HouseKeeper ('Hanuma', true, 3, ['bathroom', 'hall']);
